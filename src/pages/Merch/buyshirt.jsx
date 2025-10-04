262b14ba65bb520b9d86b2519710f43e080c9dc7
Abbas Raza <abbasrazaa02@gmail.com>
2025-10-04 16:57:24 +0500

    Deployment Updated

--- src/pages/Merch/buyshirt.jsx
+++ src/pages/Merch/buyshirt.jsx
@@ -196,7 +196,6 @@ const BuyShirt = () => {
                     justifyContent: 'center'
                   }}
                 >
-                
                   {size}
                 </Button>
               ))}
