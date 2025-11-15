480adb1f1d6308a1f9e95e61617fcfda646bceff
ali432003 <github@dotclickllc.com>
2025-11-15 19:02:07 +0500

    fix3

--- src/configs/axios.config.js
+++ src/configs/axios.config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 // export const baseURL = "http://localhost:8000/api";
-export const baseURL = import.meta.env.VITE_API_URL;
+// export const baseURL = import.meta.env.VITE_API_URL;
+export const baseURL = "https://nodejs-v0jm-production.up.railway.app/api";
 
 const custAxios = axios.create({
   baseURL: baseURL,
